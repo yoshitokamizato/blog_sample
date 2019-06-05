cd9f9f29708e747241c1551b625efd7100247a82
Yoshito Kamizato <yuyueureka157@gmail.com>
2019-06-05 22:17:51 +0900

    add users controller and views

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,17 +1,19 @@
 class BlogsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]  # deviseのメソッドで「ログインしていないユーザーをログイン画面に送る」メソッド
+
   def index
-
-  end
-
-  def show
-
+    @blogs = Blog.all
   end
 
   def new
-
+    @blog = Blog.new # 新規投稿用の空のインスタンス
   end
 
   def create
-
   end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
 end
